87013b7e340a6de04fb45b9486984114f915bcab
google110110 <wxuanwxwx@gmail.com>
2020-02-19 18:10:00 +0800

    acfun

---
+++ src/components/footer/index.vue
@@ -0,0 +1,351 @@
+<template>
+    <div>
+        <div class="main">
+			<div class="main-div">
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<div class="div-img1">
+					<div class="diva">
+						<div><img src="" alt=""></div>
+						<div>
+							<ul class="ul1">
+								<li><img src="" alt="">1</li>
+								<li><img src="" alt="">2</li>
+								<li><img src="" alt="">3</li>
+								<li><img src="" alt="">4</li>
+								<li><img src="" alt="">5</li>
+								<li><img src="" alt="">6</li>
+							</ul>
+						</div>
+					</div>
+					<div  class="divb">
+						<ul class="ul2">
+							<li>1</li>
+							<li>2</li>
+							<li>3</li>
+							<li>4</li>
+							<li>5</li>
+						</ul>
+					</div>
+				</div>
+				<!-- <div>2</div>
+				<div>3</div>
+				<div>4</div>
+				<div>5</div>
+				<div>6</div>
+				<div>7</div>
+				<div>8</div> -->
+			</div>
+		</div>
+
+		<aside>
+		</aside>
+		<!-- 侧栏 -->
+		<footer>
+		<div class="footer">footer</div>
+		</footer>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'footers'
+    }
+</script>
+
+<style lang="scss" scoped>
+    .main{
+	width: 1024px;
+	/*position: absolute;*/
+	justify-content: center;
+	display: flex;
+	width: 100%;
+	/*padding: 0px 10%;*/
+	
+
+}
+.main .main-div{
+	text-align: center;
+	width: 830px;
+	/*background: red;*/
+	border: 1px solid red;
+}
+.main .main-div>div{
+	/*display: block;*/
+	width: 820px;
+	/*box-sizing: border-box;*/
+	padding: 5px;
+	height: 400px;
+	border: 1px solid blue;
+	/*background-color: red;*/
+}
+.diva{
+	margin-bottom: 10px;
+	width: 820px;
+	overflow:hidden;
+	height: 260px;
+	display: flex;
+	margin-top: 0 0 10px 0;
+}
+.diva div:nth-child(1){
+	height: 260px;
+	width: 328px;
+	background-color: red;
+	/*display: inline;*/
+}
+.diva div:nth-child(2){
+	height: 260px;
+	width: 492px;
+}
+.ul1{
+	/*display: flex;*/
+	margin:0px;
+    padding:0px;
+	list-style-type: none;
+	display: inline-block;
+	/*float: left;*/
+	width: 492px;
+	height: 260px;
+	justify-content: space-between;
+}
+.ul1 li{
+	/*min-width: 80px;*/
+	/*background: blue;*/
+	// display: inline-block;
+	border: 1px solid red;
+	width: 162px;
+	height: 128px;
+	text-align: center;
+	float:left;
+}
+.divb{
+	width: 820px;
+	overflow:hidden;
+	height: 130px;
+	display: flex;
+}
+.ul2{
+	/*background: blue;*/
+	/*display: flex;*/
+	margin:0px;
+    padding:0px;
+	list-style-type: none;
+	display: inline-block;
+	/*float: left;*/
+	width: 820px;
+	height: 130px;
+	justify-content: space-between;
+}
+.ul2 li{
+	/*min-width: 80px;*/
+	/*background: blue;*/
+	display: inline-block;
+	border: 1px solid red;
+	width: 162px;
+	height: 128px;
+	text-align: center;
+	float:left;
+}
+footer{
+	width: 100%;
+	display: flex;
+	justify-content: center;
+}
+.footer{
+	
+	width: 1024px;
+	height: 100px;
+	border: 1px solid red;
+	/*position: absolute;*/
+}
+</style>
---
+++ src/router/acfun/index.js
@@ -0,0 +1,4 @@
+export default {
+    path:'/acfun',
+    component:()=>import('@/views/acfun')
+}
