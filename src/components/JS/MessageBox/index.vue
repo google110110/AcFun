a0914fa7a1387ba7b932e55e2d47274834dccf4c
google110110 <wxuanwxwx@gmail.com>
2020-02-21 20:54:34 +0800

    add messsagebox

---
+++ src/components/JS/MessageBox/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="message">
+        <h2>{{title}}</h2>
+        <h3>{{content}}</h3>
+        <div>
+            <div @click="handleCancel">{{cancel}}</div>
+            <div @click="handleOk">{{ok}}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'MesssageBox'
+    }
+</script>
+
+<style lang="scss" scoped>
+.message{cursor: pointer;width: 200px;height: 115px; position: absolute;top: 30%;left: 50%; border: 1px solid wheat;border-radius: 5px; box-shadow:3px 3px 3px 3px wheat;padding: 0;}
+.message h2{text-align: center; width: 200px; font-size: 20px;line-height: 30px; margin:5px 0;height: 30px;}
+.message h3{text-align: center; width: 200px; font-size: 15px;line-height: 30px; margin:5px 0;height: 30px;}
+.message >div{display:flex;width: 200px;height: 40px; border-top: 1px solid #ffffff;border-top: 1px solid #ccc;margin: 0;}
+.message div div{text-align: center;flex: 1;font-size: 15px;line-height: 40px; height: 40px}
+.message div :first-child{border-right: 1px solid #ccc;}
+</style>
---
+++ src/views/good/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+    </div>
+</template>
+
+<script>
+    import {messageBox} from '@/components/JS'
+    export default {
+        name:"good",
+        mounted(){
+            messageBox({
+                title:'自定义JS提示框',
+                content:'你好啊!',
+                cancel:'滚',
+                ok:'你好',
+                handleCancel(){
+                    console.log("滚")
+                },
+                handleOk(){
+                    console.log("你好")
+                }
+            })
+        },
+        mothods:{
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
